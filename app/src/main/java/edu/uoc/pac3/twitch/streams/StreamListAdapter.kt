2edbb4e056bb6ee75e52516835f73a6e6fcb351a
Mireia Scholz <mscholzdiaz@gmail.com>
2020-12-06 16:29:31 +0000

    Ejercicio 3 stream list first approach

---
+++ app/src/main/java/edu/uoc/pac3/twitch/streams/StreamListAdapter.kt
@@ -0,0 +1,63 @@
+package edu.uoc.pac3.twitch.streams
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import edu.uoc.pac3.R
+import edu.uoc.pac3.data.streams.Stream
+
+class StreamListAdapter(private var streams: List<Stream>) :
+    RecyclerView.Adapter<StreamListAdapter.ViewHolder>() {
+
+    private fun getStream(position: Int): Stream {
+        return streams[position]
+    }
+
+    fun setStreams(streams: List<Stream>) {
+        this.streams = streams
+        // Reloads the RecyclerView with new adapter data
+        notifyDataSetChanged()
+    }
+
+    // Creates View Holder
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.stream_list, parent, false)
+        return ViewHolder(view)
+    }
+
+    // Binds re-usable View for a given position
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val stream = getStream(position)
+        holder.titleView.text = stream.title
+        holder.usernameView.text = stream.userName
+
+        // TODO: Detalles del stream
+//        holder.view.setOnClickListener { view ->
+//            stream.uid?.let { uid ->
+//                val intent = Intent(view.context, BookDetailActivity::class.java).apply {
+//                    putExtra(BookDetailFragment.ARG_ITEM_ID, uid)
+//                }
+//                view.context.startActivity(intent)
+//                (view.context as Activity).overridePendingTransition(
+//                    R.anim.translate_in_bottom,
+//                    R.anim.translate_in_top
+//                )
+//            }
+//        }
+
+    }
+
+    // Returns total items in Adapter
+    override fun getItemCount(): Int {
+        return streams.size
+    }
+
+    // Holds an instance to the view for re-use
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val titleView: TextView = view.findViewById(R.id.title)
+        val usernameView: TextView = view.findViewById(R.id.username)
+    }
+
+}
--- app/src/main/java/edu/uoc/pac3/twitch/streams/StreamsActivity.kt
+++ app/src/main/java/edu/uoc/pac3/twitch/streams/StreamsActivity.kt
@@ -2,22 +2,40 @@ package edu.uoc.pac3.twitch.streams
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import edu.uoc.pac3.R
+import edu.uoc.pac3.data.streams.Stream
 
 class StreamsActivity : AppCompatActivity() {
 
     private val TAG = "StreamsActivity"
+    private lateinit var adapter: StreamListAdapter
+    private var streams: MutableList<Stream> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_streams)
         // Init RecyclerView
         initRecyclerView()
+
         // TODO: Get Streams
+        //getStreams()
     }
 
     private fun initRecyclerView() {
-        // TODO: Implement
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        // Set Layout Manager
+        val layoutManager: RecyclerView.LayoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+        // Init Adapter
+        streams = mutableListOf(Stream("AuronPlay", "GTA Roleplay"), Stream("Rubius", "Just chatting"))
+        adapter = StreamListAdapter(streams)
+        recyclerView.adapter = adapter
+    }
+
+    private fun getStreams() {
+        TODO("Not yet implemented")
     }
 
 }
