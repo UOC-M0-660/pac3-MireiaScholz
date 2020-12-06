ccfc237e4695ed5c2b15153e4bd27bb752a6fe33
Mireia Scholz <mscholzdiaz@gmail.com>
2020-12-06 13:35:12 +0000

    Ejercicio 2

--- app/src/main/java/edu/uoc/pac3/oauth/OAuthActivity.kt
+++ app/src/main/java/edu/uoc/pac3/oauth/OAuthActivity.kt
@@ -9,6 +9,7 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import edu.uoc.pac3.R
+import edu.uoc.pac3.data.SessionManager
 import edu.uoc.pac3.data.TwitchApiService
 import edu.uoc.pac3.data.network.Endpoints
 import edu.uoc.pac3.data.network.Network.createHttpClient
@@ -17,15 +18,6 @@ import edu.uoc.pac3.data.oauth.OAuthConstants.redirectUri
 import edu.uoc.pac3.data.oauth.OAuthConstants.scopes
 import edu.uoc.pac3.data.oauth.OAuthConstants.uniqueState
 import edu.uoc.pac3.data.oauth.OAuthTokensResponse
-import io.ktor.client.*
-import io.ktor.client.engine.okhttp.*
-import io.ktor.client.features.*
-import io.ktor.client.features.json.*
-import io.ktor.client.features.json.serializer.*
-import io.ktor.client.features.logging.*
-import io.ktor.client.request.*
-import io.ktor.http.*
-import io.ktor.http.ContentType.Application.Json
 import kotlinx.android.synthetic.main.activity_oauth.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -93,12 +85,13 @@ class OAuthActivity : AppCompatActivity() {
         progressBar.visibility = View.VISIBLE
 
         val twitch = TwitchApiService(createHttpClient(this))
+        val session = SessionManager(this)
 
         GlobalScope.launch {
             val tokens: OAuthTokensResponse? = twitch.getTokens(authorizationCode)
             if (tokens != null){
-                Log.d("OAuth", "About to save tokens ${tokens.accessToken}")
-                // TODO: Save access token and refresh token using the SessionManager class
+                session.saveAccessToken(tokens.accessToken)
+                tokens.refreshToken?.let { session.saveRefreshToken(it) }
             }
         }
 
